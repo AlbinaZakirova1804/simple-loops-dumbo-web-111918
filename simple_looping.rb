c5b669cf9853a39888c214adea2816200fd7eb41
Albina Zakirova <zak1804@gmail.com>
2018-11-08 14:50:28 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,6 +30,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
+  binding.pry
   for ithem in 1..number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
   end
